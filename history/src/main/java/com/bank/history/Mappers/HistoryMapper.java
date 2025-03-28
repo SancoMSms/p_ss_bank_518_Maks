142d702db6e6a7f75bc433ff1d1f55c250b718b4
Mikayel <m.13021999@mail.ru>
2025-03-28 20:52:08 +0300

    @Mapper(componentModel = "spring")

--- history/src/main/java/com/bank/history/Mappers/HistoryMapper.java
+++ history/src/main/java/com/bank/history/Mappers/HistoryMapper.java
@@ -1,12 +1,13 @@
 package com.bank.history.Mappers;
 
 
+import ch.qos.logback.core.model.ComponentModel;
 import com.bank.history.DTO.HistoryDto;
 import com.bank.history.Entities.History;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface HistoryMapper {
     HistoryMapper INSTANCE = Mappers.getMapper(HistoryMapper.class);
 
