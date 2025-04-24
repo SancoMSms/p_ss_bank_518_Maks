9da807b15cd1cf2211c31376e7e9121c6de6f9af
SancoMSms <masdakket2006@gmail.com>
2025-04-24 11:00:09 +0300

    Commit

--- profile/src/test/java/com/bank/profile/Kafka/KafkaErrorProducerTest.java
+++ profile/src/test/java/com/bank/profile/Kafka/KafkaErrorProducerTest.java
@@ -8,7 +8,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class KafkaErrorProducerTest {
 
--- profile/src/test/java/com/bank/profile/Services/RegistrationServiceImplTest.java
+++ profile/src/test/java/com/bank/profile/Services/RegistrationServiceImplTest.java
@@ -19,7 +19,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.contains;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RegistrationServiceImplTest {
